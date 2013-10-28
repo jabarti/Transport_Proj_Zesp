a6723612bef37f1ef2cf879e94bbe9bf6616ef1a
Bartosz Lewinski <jabarti@wp.pl>
2013-10-28 20:59:08 +0100

    dodatki

---
+++ ExternalResources/DBChanges.sql
@@ -0,0 +1,24 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+DROP SCHEMA IF EXISTS `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Osoba`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mydb`.`Osoba` ;
+
+CREATE  TABLE IF NOT EXISTS `mydb`.`Osoba` (
+  `idOsoba` INT NOT NULL ,
+  PRIMARY KEY (`idOsoba`) )
+ENGINE = InnoDB;
+
+USE `mydb` ;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
